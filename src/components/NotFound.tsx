ae7c686e672b1b44c99c8329b147a4e6cd9632d7
Alex Revell <alex.revell@abx.com>
2018-05-21 16:27:41 +1000

    Add more packages, sass building, initial front page from bulma basic cover

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default function NotFound (props: any) {
+  return (
+    <div className='container'>
+      <h1>Page Not Found</h1>
+    </div>
+  )
+}
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.scss'
+
+import { App } from './components';
+
+ReactDOM.render(<App />, document.getElementById('root'));
