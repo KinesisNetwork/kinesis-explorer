255e2ed8499b9e2df87fa6b6fb71cbe6b773c9ce
Carl Foster <carl.john.foster@gmail.com>
2018-06-15 10:38:33 +1000

    Add EventSource polyfill for Edge

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+
+import 'event-source-polyfill/src/eventsource.min.js'
 import './index.scss'
 
 import { App } from './components'
