d5de0a12bb2d812a972b3a3a60dd39dd44e4668a
Jeff Teng <jeff.teng@abx.com>
2019-03-12 14:30:45 +1000

    🐛 Fix: Fix the timeout message even the fetch is done.
    Timeout error was always showing even the inflation operation is fetch. Fixed the problem

--- src/services/connections.ts
+++ src/services/connections.ts
@@ -1,9 +1,15 @@
 import { Connection } from '../types'
 
 export async function fetchConnections(): Promise<Connection[]> {
-  const response = await fetch('https://s3-ap-southeast-2.amazonaws.com/kinesis-config/kinesis-server-details.json')
+  const response = await fetch(
+    'https://s3-ap-southeast-2.amazonaws.com/kinesis-config/kinesis-server-details.json',
+  )
   const connections: Connection[] = await response.json()
   return connections
 }
 
-export { ConnectionContext, ConnectionContainer, ConnectionContextHandlers } from './ConnectionContext'
+export {
+  ConnectionContext,
+  ConnectionContainer,
+  ConnectionContextHandlers,
+} from './ConnectionContext'
