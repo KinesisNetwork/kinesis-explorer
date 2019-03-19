06c2d6a9bc0cb249cba3ce22ff8f1b793977c907
Alex Revell <alex.revell@abx.com>
2019-03-19 14:02:42 +1000

    ABX-4970 remove unneeded js-kinesis-sdk type references after sdk version update

--- src/types/js-kinesis-sdk.d.ts
+++
@@ -1,9 +0,0 @@
-import { CallCollectionFunction, TransactionRecord } from "js-kinesis-sdk";
-
-declare module "js-kinesis-sdk" {
-  interface AccountRecord {
-    transactions: CallCollectionFunction<TransactionRecord>
-  }
-
-  interface AccountResponse extends AccountRecord { }
-}
