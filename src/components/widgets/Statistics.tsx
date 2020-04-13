98e21bdf8d3970f56a0aee4e9b125443ab66095e
Borislav Shekerov <shekerovborislav@gmail.com>
2020-04-13 16:24:04 +0300

    Enabling fee-pool request in test environments

--- src/components/widgets/Statistics.tsx
+++ src/components/widgets/Statistics.tsx
@@ -1,22 +1,27 @@
-import { LedgerRecord } from 'js-kinesis-sdk'
-import * as React from 'react'
-import { Subscribe } from 'unstated'
+import { LedgerRecord } from "js-kinesis-sdk";
+import * as React from "react";
+import { Subscribe } from "unstated";
+import Decimal from "decimal.js";
 
 import {
   ConnectionContainer,
   ConnectionContext,
-} from '../../services/connections'
-import { getLedgers } from '../../services/kinesis'
-import { getBackedFees, getKMSCurrencyFees, getUnbackedBalances } from '../../services/statistics'
-import { Connection } from '../../types'
-import { renderAmount } from '../../utils'
-import { HorizontalLabelledField } from '../shared/LabelledField'
+} from "../../services/connections";
+import { getLedgers } from "../../services/kinesis";
+import {
+  getBackedFees,
+  getKMSCurrencyFees,
+  getUnbackedBalances,
+} from "../../services/statistics";
+import { Connection } from "../../types";
+import { renderAmount } from "../../utils";
+import { HorizontalLabelledField } from "../shared/LabelledField";
 
-type StatisticsWidgetProps = ConnectionContext
+type StatisticsWidgetProps = ConnectionContext;
 interface State {
-  totalFeePool: number
-  totalInCirculation: number
-  isLoading: boolean
+  totalFeePool: number;
+  totalInCirculation: number;
+  isLoading: boolean;
 }
 
 class StatisticsWidget extends React.Component<StatisticsWidgetProps, State> {
@@ -24,75 +29,75 @@ class StatisticsWidget extends React.Component<StatisticsWidgetProps, State> {
     totalFeePool: 0,
     totalInCirculation: 0,
     isLoading: false,
-  }
+  };
 
   componentDidMount() {
-    this.loadStatisticsData(this.props.selectedConnection)
+    this.loadStatisticsData(this.props.selectedConnection);
   }
 
   componentDidUpdate(prevProps: StatisticsWidgetProps) {
     if (this.props.selectedConnection !== prevProps.selectedConnection) {
-      this.loadStatisticsData(this.props.selectedConnection)
+      this.loadStatisticsData(this.props.selectedConnection);
     }
   }
 
   loadStatisticsData = async (connection: Connection): Promise<void> => {
-    this.setState({ isLoading: true })
-    const { totalCoins, feePool } = await this.fetchLatestLedger(connection)
-    const unbackedBalances = await getUnbackedBalances(connection)
-    const backedFeesInPool = await getBackedFees(connection)
-    const kmsFees = await getKMSCurrencyFees(connection)
-    const ledgerFeePool = Number(feePool)
-    const unbackedFeesInPool = ledgerFeePool - backedFeesInPool
-    const totalFeePool = kmsFees + backedFeesInPool
+    this.setState({ isLoading: true });
+    const { totalCoins, feePool } = await this.fetchLatestLedger(connection);
+    const unbackedBalances = await getUnbackedBalances(connection);
+    const backedFeesInPool = await getBackedFees(connection);
+    const kmsFees = await getKMSCurrencyFees(connection);
+    const ledgerFeePool = Number(feePool);
+    const unbackedFeesInPool = ledgerFeePool - backedFeesInPool;
+    const totalFeePool = new Decimal(kmsFees).add(backedFeesInPool).toNumber();
 
     const totalInCirculation =
-      totalCoins - unbackedBalances - unbackedFeesInPool
+      totalCoins - unbackedBalances - unbackedFeesInPool;
 
     this.setState({
       totalInCirculation,
       totalFeePool,
       isLoading: false,
-    })
-  }
+    });
+  };
 
   fetchLatestLedger = async (
-    connection: Connection,
+    connection: Connection
   ): Promise<LedgerRecord & { totalCoins: number; feePool: number }> => {
-    const ledgers = await getLedgers(connection)
-    const latest = ledgers[0]
+    const ledgers = await getLedgers(connection);
+    const latest = ledgers[0];
 
     return {
       ...latest,
       totalCoins: Number(latest.total_coins),
       feePool: Number(latest.fee_pool),
-    }
-  }
+    };
+  };
 
   render() {
-    const { totalFeePool, totalInCirculation, isLoading } = this.state
+    const { totalFeePool, totalInCirculation, isLoading } = this.state;
     const {
       selectedConnection: { currency },
-    } = this.props
+    } = this.props;
     return (
-      <article className='tile is-child box'>
-        <p className='title'>Statistics</p>
+      <article className="tile is-child box">
+        <p className="title">Statistics</p>
         <div>
           <HorizontalLabelledField
-            label={'Kinesis in Circulation'}
+            label={"Kinesis in Circulation"}
             wideLabel={true}
             value={`${currency} ${renderAmount(totalInCirculation)}`}
             isLoading={isLoading}
           />
           <HorizontalLabelledField
-            label={'Total Fee Pool'}
+            label={"Total Fee Pool"}
             wideLabel={true}
             value={`${currency} ${renderAmount(totalFeePool)}`}
             isLoading={isLoading}
           />
         </div>
       </article>
-    )
+    );
   }
 }
 
@@ -102,6 +107,6 @@ export default class ConnectedStatistics extends React.Component {
       <Subscribe to={[ConnectionContainer]}>
         {({ state }: ConnectionContainer) => <StatisticsWidget {...state} />}
       </Subscribe>
-    )
+    );
   }
 }
