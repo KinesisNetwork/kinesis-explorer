234e603dbe9629292d0ee6c3609f21cef27171c7
thani324 <thani@leewayhertz.com>
2021-03-18 11:56:37 +0530

    fixes

--- src/components/layout/NodeInfo.tsx
+++ src/components/layout/NodeInfo.tsx
@@ -1,15 +1,14 @@
 import axios from 'axios'
 import * as React from 'react'
 import '../css/custom.css'
+import AbxLogo from '../css/images/ABX_logo.svg'
 import KagIcon from '../css/images/kag-icon.svg'
 import KauIcon from '../css/images/kau-icon.svg'
 import KemIcon from '../css/images/kem-icon.svg'
 import KinesisLogo from '../css/images/KinesisIcon.svg'
 import LeewayLogo from '../css/images/LH.svg'
-import AbxLogo from '../css/images/ABX_logo.svg'
 
 const REGION_ERROR = { error: 'Region Offline' }
-
 
 function getInfo(ep: string) {
   return axios
@@ -18,26 +17,20 @@ function getInfo(ep: string) {
     .catch(() => REGION_ERROR)
 }
 
-interface endpoints {
-  [n: string] : {
-    nodeUrl : string,
-    account : string
-  }
-}
 
 async function loadData() {
-  const nodesData = await axios.get("https://kinesis-config.s3-ap-southeast-2.amazonaws.com/kinesis-explorer-uat.json").then((d) => d.data) 
+  const nodesData = await axios.get('https://kinesis-config.s3-ap-southeast-2.amazonaws.com/kinesis-explorer-uat.json').then((d) => d.data)
   const data = await Promise.all(
-    Object.entries(nodesData).map(async ([environment, endpoints] : any) => {
-    
+    Object.entries(nodesData).map(async ([environment, endpoints]: any) => {
+
       const envInfo = await Promise.all(
-        endpoints.map(async (ep : any) => {
+        endpoints.map(async (ep: any) => {
           const info = await getInfo(ep.nodeUrl)
           // info.account = info.account
           return { [ep.nodeUrl]: { ...info , account: ep.account} }
         }),
       )
-      const mergedEnvInfo = envInfo.reduce((acc : any, val : any) => {
+      const mergedEnvInfo = envInfo.reduce((acc: any, val: any) => {
          return { ...acc, ...val }
       } , {})
       return { [environment]: mergedEnvInfo }
@@ -66,10 +59,10 @@ export default class NodeInfo extends React.Component<any, { nodeInfo: any; inte
 
   public generateHeader(network) {
     return (
-      <div className="header-info display-flex-outer" >
-        <div className="display-flex right-header">
+      <div className='header-info display-flex-outer' >
+        <div className='display-flex right-header'>
           <div>
-            <img src={network.includes("kau") ? KauIcon : network.includes("kag") ? KagIcon : KemIcon} className="image-icon" />
+            <img src={network.includes('kau') ? KauIcon : network.includes('kag') ? KagIcon : KemIcon} className='image-icon' />
           </div>
           <div >
             <h1 className='text-data'>{network}</h1>
@@ -117,24 +110,22 @@ export default class NodeInfo extends React.Component<any, { nodeInfo: any; inte
     const regions = Object.keys(networkRegionInfo)
     return regions.map((region) => {
       const regionNodeInfo = networkRegionInfo[region]
-      let regionArea = region.split('-')[2].split('.')[0]
+      const regionArea = region.split('-')[2].split('.')[0]
 
-      let { account } = regionNodeInfo
+      const { account } = regionNodeInfo
 
       return (
         <React.Fragment key={region}>
           <div className='column node-info-details '>
-            <div className="region-header">
-              {/* <h2 className='title is-4'>{region}</h2> */}
+            <div className='region-header'>
               <div className='display-space'>
-                <img src={account == "Kinesis" ?KinesisLogo : account == "leewayhertz" ? LeewayLogo : AbxLogo} className="image-icon icon-padding" />
-                {/* <p>{account}</p> */}
+                <img src={account == 'Kinesis' ? KinesisLogo : account == 'leewayhertz' ? LeewayLogo : AbxLogo} className='image-icon icon-padding' />
               </div>
             </div>
             {regionNodeInfo.error === REGION_ERROR.error ? (
               <h3 className='title is-5 has-text-danger'>Region Offline</h3>
             ) : (
-              this.generateNodeView(regionNodeInfo , regionArea ,region)
+              this.generateNodeView(regionNodeInfo , regionArea , region)
             )}
           </div>
         </React.Fragment>
@@ -142,13 +133,13 @@ export default class NodeInfo extends React.Component<any, { nodeInfo: any; inte
     })
   }
 
-  public generateNodeView(regionNodeInfo: any , regionArea : string , region : string) {
+  public generateNodeView(regionNodeInfo: any , regionArea: string , region: string) {
     const nodes = Object.keys(regionNodeInfo)
 
     return nodes.map((node) => {
       const currentNode = regionNodeInfo[node]
 
-      if(typeof currentNode === 'object') {
+      if (typeof currentNode === 'object') {
         const { info } = currentNode
         const { quorum, ledger, state, protocol_version } = info
         const qSet = () => {
@@ -158,24 +149,24 @@ export default class NodeInfo extends React.Component<any, { nodeInfo: any; inte
             return Object.values(quorum)[0] as any
           }
         }
-  
+
         const { agree } = quorum ? qSet() : { agree: 0 }
         return (
           <React.Fragment key={node}>
-            <div className="individual-indetails">
+            <div className='individual-indetails'>
               <h4 className='title is-4' style={{ paddingTop: '15px' }}>
-               <a target="_blank" href={region}>{regionArea.includes('0') || regionArea.includes('1') || regionArea.includes('2') || regionArea.includes('3') ? regionArea : regionArea +  " "  +node[4]}</a>
+               <a target='_blank' href={region}>{regionArea.includes('0') || regionArea.includes('1') || regionArea.includes('2') || regionArea.includes('3') ? regionArea : regionArea +  ' '  + node[4]}</a>
               </h4>
-              <p className="para-text">State: <span className="font-bolder">{state}</span></p>
-              <p className="para-text">
+              <p className='para-text'>State: <span className='font-bolder'>{state}</span></p>
+              <p className='para-text'>
                 Quorum Count:
               {/* <span className={agree < 12 ? 'has-text-danger' : ''}>{agree}</span> */}
-                <span className="font-bolder"> {agree}</span>
+                <span className='font-bolder'> {agree}</span>
               </p>
-              <p className="para-text">Ledger Age: <span className="font-bolder" >{ledger.age}</span></p>
-              <p className="para-text">Ledger Number:  <span className="font-bolder" >{ledger.num}</span></p>
-              <p className="para-text">Ledger Percentage Fee (b.p): <span className="font-bolder" >{ledger.basePercentageFee || 45}</span></p>
-              <p className="para-text">Ledger Base Fee (stroops): <span className="font-bolder" >{ledger.baseFee}</span></p>
+              <p className='para-text'>Ledger Age: <span className='font-bolder' >{ledger.age}</span></p>
+              <p className='para-text'>Ledger Number:  <span className='font-bolder' >{ledger.num}</span></p>
+              <p className='para-text'>Ledger Percentage Fee (b.p): <span className='font-bolder' >{ledger.basePercentageFee || 45}</span></p>
+              <p className='para-text'>Ledger Base Fee (stroops): <span className='font-bolder' >{ledger.baseFee}</span></p>
             </div>
           </React.Fragment>
         )
