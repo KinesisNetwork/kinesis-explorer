220e339a485dddde9cd7abe231254c5b1234f10b
ishuchawla <ishu@leewayhertz.com>
2021-03-19 19:05:30 +0530

    Travis Build Error Resolve

--- src/services/kinesis.ts
+++ src/services/kinesis.ts
@@ -12,33 +12,34 @@ import {
 import { Connection } from '../types'
 
 import {
+  AccountDetails,
+  Environment,
   KinesisBlockchainGatewayFactory,
-  KinesisCoin, Environment , LedgerRecord as LedgerRecordNew, TransactionRecord as TransactionRecordNew , AccountDetails
+  KinesisCoin,
+  LedgerRecord as LedgerRecordNew,
+  TransactionRecord as TransactionRecordNew,
 } from '@abx/js-kinesis-sdk-v2'
-
 
 const STROOPS_IN_ONE_KINESIS = 1e7
 
-function getCoinEnviron(connection){
-  let res;
-  if (connection.currency == "KAG") {
+function getCoinEnviron(connection) {
+  let res
+  if (connection.currency === 'KAG') {
     res = KinesisCoin.KAG
-  } 
-  else if (connection.currency == "KAU") {
+  } else if (connection.currency === 'KAU') {
     res = KinesisCoin.KAU
   } else {
     res = KinesisCoin.KEM
   }
-  console.log(res , "===")
-  let environ;
-  if (connection.stage === "mainnet") {
+  let environ
+  if (connection.stage === 'mainnet') {
     environ = Environment.mainnet
   } else {
     environ = Environment.testnet
   }
   return {
     res,
-    environ
+    environ,
   }
 }
 
@@ -57,7 +58,6 @@ export function getServer(connection: Connection): Server {
 }
 
 export async function getTransaction(connection: Connection, transactionId: string) {
-
   const server = getServer(connection)
   return await server.transactions().transaction(transactionId).call()
 }
@@ -68,7 +68,6 @@ export async function getTransactions(
   limit = 10,
   cursor?: string,
 ): Promise<TransactionRecord[]> {
-
   const server = getServer(connection)
   const transactionsPromise = server.transactions()
 
@@ -95,11 +94,12 @@ export async function getTransactionStream(
 }
 
 export async function getLedger(connection: Connection, sequence: number | string): Promise<LedgerRecordNew> {
-
-  let response =  getCoinEnviron(connection)
-  let blockchainGateway = await new KinesisBlockchainGatewayFactory().getGatewayInstance(response.res, response.environ)
-   return  blockchainGateway.getLedgerWithSequenceNumber(sequence)
-  
+  const response = getCoinEnviron(connection)
+  const blockchainGateway = await new KinesisBlockchainGatewayFactory().getGatewayInstance(
+    response.res,
+    response.environ,
+  )
+  return blockchainGateway.getLedgerWithSequenceNumber(sequence)
 }
 
 export async function getLedgers(connection: Connection, limitVal: number = 10): Promise<LedgerRecord[]> {
@@ -114,11 +114,12 @@ export async function getLedgerStream(connection: Connection, cursor = 'now'): P
 }
 
 export async function getAccount(connection: Connection, accountId: string): Promise<AccountDetails> {
-  
-  let response =  getCoinEnviron(connection)
-  let blockchainGateway = await new KinesisBlockchainGatewayFactory().getGatewayInstance(response.res, response.environ)
-   return  blockchainGateway.getAccount(accountId)
-
+  const response = getCoinEnviron(connection)
+  const blockchainGateway = await new KinesisBlockchainGatewayFactory().getGatewayInstance(
+    response.res,
+    response.environ,
+  )
+  return blockchainGateway.getAccount(accountId)
 }
 
 export async function validateAccount(address: string): Promise<boolean> {
