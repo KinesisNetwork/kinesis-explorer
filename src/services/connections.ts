c2bcc2e3d63ba2b5b85e18e470e0639730f3ebef
D123605 <debarishi@leewayhertz.com>
2021-05-25 14:53:59 +0530

    S3 URL updated for UAT

--- src/services/connections.ts
+++ src/services/connections.ts
@@ -3,14 +3,14 @@ import { Connection } from '../types'
 export async function fetchConnections(): Promise<Connection[]> {
 
   // for live
-  const response = await fetch(
-    'https://s3-ap-southeast-2.amazonaws.com/kinesis-config/kinesis-server-details.json',
-  )
+  // const response = await fetch(
+  //   'https://s3-ap-southeast-2.amazonaws.com/kinesis-config/kinesis-server-details.json',
+  // )
 
   // for UAT
-  // const response = await fetch(
-  //   'https://kinesis-config.s3-ap-southeast-2.amazonaws.com/kinesis-server-details-uat.json',
-  // )
+  const response = await fetch(
+     'https://kinesis-config.s3-ap-southeast-2.amazonaws.com/kinesis-server-details-uat.json',
+   )
 
   const connections: Connection[] = await response.json()
   return connections
